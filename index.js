cbe914fb1c3788a9c31fdcfff0b9562fdaab52f4
Arokh-web <michaelabudich@gmail.com>
2025-04-26 15:27:30 +0200

    update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ import postRouter from "./routes/postRoute.js";
 // Initializing App and DB
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.API_PORT;
 let db;
 
 tryConnections()
